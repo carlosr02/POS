8159c3425f96cdbe56749fd870d98623595ae063
carlosr02 <carlosr-02@live.com>
2017-05-10 16:22:29 -0300

    node modelus

--- arquivos_texto/exemplo_csv.js
+++ arquivos_texto/exemplo_csv.js
@@ -6,9 +6,10 @@ var resultado;
 
 fs.readFile(arquivo, 'utf8', (err, data) => {
     resultado = csv.parse(data)
-    console.log(resultado[1][0])
+    console.log(resultado)
+    /*console.log(resultado[1][0])
     console.log(resultado[1][1])
     console.log(resultado[1][2])
     console.log(resultado[1][3])
-    console.log(resultado[1][4])
+    console.log(resultado[1][4])*/
 });
